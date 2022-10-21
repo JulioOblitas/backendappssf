e6ae6ecbbf92dede3de82c89812536fa97a7628e
JulioOblitas <89611439+JulioOblitas@users.noreply.github.com>
2022-10-21 10:16:08 -0500

    cambios 211022

--- src/services/producto.service.js
+++ src/services/producto.service.js
@@ -7,11 +7,15 @@ let datos;
     password: 'nitro2020',
     database:'data02'*/
   
-    host: '208.115.213.75',
+    /*host: '208.115.213.75',
     user: 'agrouser',
     password: '@groUs3r_',
-    database:'agrovado_qa'
-  
+    database:'agrovado_qa'*/
+   host: 'qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+   user: 'ddvj6ahcpzp1cuw1',
+   password: 'fx0hhjemj5i7q3a5',
+   database: 'sez5lgvewaidh34f'
+
   
   })
   conexion.connect (err  => {
@@ -28,7 +32,7 @@ let datos;
 
     
     static async listaProducto(){
-       conexion.query("select n_id, c_despro,n_stkact  from alm_inventario where n_idfam = 16",function (err, result) {
+       conexion.query("select n_id, c_despro  from pro_productos where n_idfam = 16",function (err, result) {
 
           if (err){ 
           console.log(err.message); // <= mostramos el mensaje en consola        
